773c1a41913ac40d28e4d22fc2700acc750bb022
SirJacobB <jacob.brillante.te15d@gmail.com>
2017-11-08 13:27:09 +0100

    created constructor, added size and colour

--- src/test_repo/Player.java
+++ src/test_repo/Player.java
@@ -1,10 +1,14 @@
 package test_repo;
 
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 public class Player extends Circle{
 
-	public Player() {
+	public Player(double radius) {
 		
+		this.setRadius(radius);
+		
+		this.setFill(Color.rgb((int)(Math.random()*255), (int)(Math.random()*255), (int)(Math.random()*255)));
 	}
 }
